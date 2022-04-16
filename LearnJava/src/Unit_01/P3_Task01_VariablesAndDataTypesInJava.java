f2bfd68151fa2c692d3d8ff6949d307622d475f6
MI Computer <MI Computer@DESKTOP-PCU1D4T>
2022-04-16 12:04:45 +0530

    First Commit

---
+++ LearnJava/src/Unit_01/P3_Task01_VariablesAndDataTypesInJava.java
@@ -0,0 +1,28 @@
+package Unit_01;
+
+public class P3_Task01_VariablesAndDataTypesInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int     a = 10;
+		float   b = 20.56f;
+		char    c = 'D';
+		double  d = 24.622d;
+		boolean e = true;
+		
+		//This way these local variables can be printed.
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		//As static variables do not require to be called through an instance of class. SO , it can be accessed(preferably) by:>
+		System.out.println( Variable.ch );
+	}
+}
+
+class Variable{
+	static char ch = 'A';
+
+	}
